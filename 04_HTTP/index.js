380f336ab4917b5a9e8149592c7bac9924abfce5
mauriciobrito7 <maubot01@gmail.com>
2018-04-17 19:31:20 -0400

    instalando mongoose para hacer la conexión a la base de datos

--- 04_HTTP/index.js
+++ 04_HTTP/index.js
@@ -2,8 +2,10 @@
 
 // Requerimos el modulo de express
 /* Importamos un modulo con la palabra reservada require */
-const express = require("express");
+const express = require('express');
 const app = express();
+// mongoose tiene un método llamado connect que le pasamos un string donde se encuentra nuestra base de datos
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 // --Middlewares--
 
@@ -35,7 +37,17 @@ app.delete('/api/product/:productId', (req,res)=> {
 
 });
 
-// Levantamos el servidor
-app.listen(port, () => {
-  console.log(`API REST corriendo en http://localhost:${port}`);
+/* Nos interesa tener primero levantada nuestra conexión con la base de datos*/
+mongoose.connect('mongodb://localhost:27017/shop',(err, res)=> {
+  if ( err ) {
+    return console.log(`Error al conectarse a la base de datos ${err}`);
+  }
+  console.log('Conexión a la base de datos establecida');
+  /* Despues que hacemos la conexión con la base de datos levantamos nuestra API*/
+  // Levantamos el servidor
+  app.listen(port, () => {
+    console.log(`API REST corriendo en http://localhost:${port}`);
+  });
 });
+
+
